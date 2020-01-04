1caaadb4b6a4b1ad1b0cc8fa8d56d06449797289
liumingzhou <18231133236@163.com>
2020-01-05 00:12:08 +0800

    demo

---
+++ demo/app.js
@@ -0,0 +1,27 @@
+const Koa = require('koa');
+
+const router = require('koa-router')();
+
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+    await next(); // 调用下一个middleware
+});
+
+app.use(async (ctx, next) => {
+    const start = new Date().getTime(); // 当前时间
+    await next(); // 调用下一个middleware
+    const ms = new Date().getTime() - start; // 耗费时间
+    console.log(`Time: ${ms}ms`); // 打印耗费时间
+});
+
+app.use(async (ctx, next) => {
+    // await next();
+    console.log('res')
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
+});
+
+app.listen(3000);
+console.log('app started at port 3000')
